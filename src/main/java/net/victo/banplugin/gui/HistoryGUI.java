598dc0a46b15b55e69f5e6ea594ab79ca8760c75
Victor Betoni <victorbetoni@protonmail.com>
2022-12-24 00:52:46 -0300

    Shadowing threader lib

---
+++ src/main/java/net/victo/banplugin/gui/HistoryGUI.java
@@ -0,0 +1,13 @@
+package net.victo.banplugin.gui;
+
+import net.threader.lib.gui.InventoryGUI;
+import org.bukkit.entity.Player;
+
+public class HistoryGUI {
+
+    private Player holder;
+
+    public HistoryGUI(Player holder) {
+        this.holder = holder;
+    }
+}
